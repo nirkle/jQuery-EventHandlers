4df11abefaa24303dbb3626cb77dcf2c8b6fe159
Nirkle <34052345+nirkle@users.noreply.github.com>
2017-12-01 20:38:09 +1100

    Add CSS

--- js/jQueryEventHandlers.js
+++ js/jQueryEventHandlers.js
@@ -1,5 +1,9 @@
 
 $(document).ready(function(){
-
-  $('footer').fadeIn(1000);
+	$('h1').animate({left:"0"},500);
+	$('footer').fadeIn(1000);
+	
+	$('.help-text').click(function(){
+		$('.help-text').text('This is a form. Please fill it out. Simple as that.');
+	});
 });
