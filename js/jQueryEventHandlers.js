7aa170c4815390528234e7d457ae73c4eae902db
Nirkle <34052345+nirkle@users.noreply.github.com>
2017-12-01 20:50:51 +1100

    Add functions

--- js/jQueryEventHandlers.js
+++ js/jQueryEventHandlers.js
@@ -1,13 +1,19 @@
 
 $(document).ready(function(){
+	var helpMessage = 'This is a form. Please fill it out. Simple as that.';
+	
 	$('h1').animate({left:"0"},500);
 	$('footer').fadeIn(1000);
 	
 	$('.help-text').click(function(){
-		$('.help-text').text('This is a form. Please fill it out. Simple as that.');
+		$('.help-text').text(helpMessage);
 	});
 	
 	$('input').focusin(function(){
-		$('input').addClass('active-input');
+		$(this).addClass('active-input');
+	});
+	
+	$('input').focusout(function(){
+		$(this).removeClass('active-input');
 	});
 });
